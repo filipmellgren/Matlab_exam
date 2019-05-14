2ebe89ef359e894dfa4816afa755fedfa79b2342
Filip <Filip@192.168.0.3>
2019-05-14 17:13:05 +0200

    2b started. Still need to solve for k.

--- main.m
+++ main.m
@@ -1,3 +1,24 @@
 % 16 May 2019
-% Adding some more stuff to this
-a = 1+1;
+%% Calibration and steady state
+% b) 
+% Values provided to us in the exercise:
+gamma = 0.5;
+theta = 0.4;
+delta = 0.05;
+h = 1/3;
+ky = 3; % k/y
+
+% Values I solved for
+r = theta/ky;
+w = (1-theta) * ky^(theta/(1-theta));
+
+v = tau*w*h;
+c = (1-tau)*w*h + r*k + v - k - (1-delta)*k; % true in steady state
+
+% Values provided by part b
+tau = 0.3;
+cbar = 0;
+
+% alpha and beta
+alpha = ((1-tau)*(1-theta)*ky^(theta/(1-theta))) / (h*(c - cbar));
+beta = 1 / (theta*(1/ky) + (1 - delta));
