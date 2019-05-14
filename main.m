f3f2cf20336ba29ea71ba7dabf8183f6ce9f1d5f
Filip <Filip@192.168.0.3>
2019-05-14 18:23:02 +0200

    Alpha is way off. Must double check. Everything is sooved for, however.

--- main.m
+++ main.m
@@ -6,19 +6,23 @@ gamma = 0.5;
 theta = 0.4;
 delta = 0.05;
 h = 1/3;
-ky = 3; % k/y
-
-% Values I solved for
-r = theta/ky;
-w = (1-theta) * ky^(theta/(1-theta));
-
-v = tau*w*h;
-c = (1-tau)*w*h + r*k + v - k - (1-delta)*k; % true in steady state
+kdivy = 3; % k/y
 
 % Values provided by part b
 tau = 0.3;
 cbar = 0;
 
+% Values I solved for
+r = theta * 1/kdivy;
+w = (1-theta) * kdivy^(theta/(1-theta));
+
+k = h / ((1/kdivy)^(1/(1-theta)));
+
+v = tau*w*h;
+c = (1-tau)*w*h + r*k + v - k + (1-delta)*k; % true in steady state
+
 % alpha and beta
-alpha = ((1-tau)*(1-theta)*ky^(theta/(1-theta))) / (h*(c - cbar));
-beta = 1 / (theta*(1/ky) + (1 - delta));
+alpha = ((1-tau)*(1-theta)*(kdivy^(theta/(1-theta)))) / (h^(1 + 1/gamma)*(c - cbar)); 
+beta = 1 / (theta*(1/kdivy) + (1 - delta)); % 0.9231
+
+% c)
