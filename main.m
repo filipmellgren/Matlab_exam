3fa0862a91449ce28af9d38b7ca7feb8f715ec6c
Filip <Filip@192.168.0.3>
2019-05-14 19:25:42 +0200

    Think 2 is solved. Have computed revenue, given a tax rate

--- main.m
+++ main.m
@@ -14,7 +14,7 @@ cbar = 0;
 
 % Values I solved for
 r = theta * 1/kdivy;
-w = (1-theta) * kdivy^(theta/(1-theta));
+w = (1-theta) * kdivy^(theta/(1-theta)); % must be wrong
 
 k = h / ((1/kdivy)^(1/(1-theta)));
 
@@ -22,7 +22,17 @@ v = tau*w*h;
 c = (1-tau)*w*h + r*k + v - k + (1-delta)*k; % true in steady state
 
 % alpha and beta
-alpha = ((1-tau)*(1-theta)*(kdivy^(theta/(1-theta)))) / (h^(1 + 1/gamma)*(c - cbar)); 
+alpha = ((1-tau)*w) / (h^(1/gamma)*(c - cbar)); % 13.34
 beta = 1 / (theta*(1/kdivy) + (1 - delta)); % 0.9231
 
 % c)
+tauvec = (0.01:0.01:0.99)'; % 1% to 99% in steps of 1%
+% solve for optimal hours worked
+hvec = ((1-tauvec)*w/(c - cbar)).^(gamma); % optimal hours worked, given tau and the others
+% solve for new capital stock using f.o.c w.r.t capital
+kvec = (theta * (hvec.^(1-theta)) /r).^(1/(1-theta));
+% solve for new wage, simply marginal product of labour
+wvec = (1-theta)*(kvec./hvec).^theta; % constant
+% Government revenue:
+tauvec.*wvec.*hvec;
+
