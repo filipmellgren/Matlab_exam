0f158f80e5faf8cee7621e6307e177307f058f66
Filip <Filip@192.168.0.3>
2019-05-16 12:03:48 +0200

    Changed this function according to the findings that y/k = psi

---
+++ hfromalpha.m
@@ -0,0 +1,21 @@
+function [h] = hfromalpha(tau, w, xi, gamma, theta, alphaval, kdivy, delta)
+%hfrom alpha is the alpha function solved for h.
+%   As solved for in exercise 2c, presented in the pdf.
+% Assumes w is exogenous to households and that kdivy is constant.
+% it's a long expression but it can be tested by giving this function the
+% argument tau = 0.3 and checking that hvec = 0.333 – which is true.
+
+ydivk = 1/kdivy; % Actually, this also happen to equal psi
+
+ % Want to avoid lengthy algebra in one expression. So I break it into
+ % parts:
+
+frac1 = ((1-tau)*w) ./ (alphaval * (1-xi));
+frac2 = 1/(ydivk^(1/(theta -1)) * (ydivk - delta));
+
+h = (frac1 .* frac2).^(1/(1+1/gamma));
+
+%h = ( (1/kdivy)^(1/(1-theta)) * (1-tau)*w / (( (1-xi)*
+%alphaval*(1/kdivy-delta))) ).^(gamma/(1+gamma)); % OLD!
+end
+
